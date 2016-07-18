b8200e378275e21b06715d7a98015f86cac42cad
fmjnax <joseph.robinson83@gmail.com>
2016-07-18 16:53:24 -0500

    Import from XNA

---
+++ Board.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TripleTriadOffline
+{
+    class Board
+    {
+        public Texture2D texture;
+        public Rectangle rect;
+        public Vector2 position;
+        Vector2 center;
+
+        public Slot[] slot = new Slot[9];
+
+        public Board()
+        {
+            slot[0] = new Slot("TL",new Rectangle(152, 114, 65, 65));
+            slot[1]= new Slot("TM",new Rectangle(219, 114, 65, 65));
+            slot[2] = new Slot("TR",new Rectangle(286, 114, 65, 65));
+            slot[3] = new Slot("ML",new Rectangle(152, 182, 65, 65));
+            slot[4] = new Slot("MM",new Rectangle(219, 182, 65, 65));
+            slot[5] = new Slot("MR",new Rectangle(286, 182, 65, 65));
+            slot[6] = new Slot("BL",new Rectangle(152, 250, 65, 65));
+            slot[7] = new Slot("BM",new Rectangle(219, 250, 65, 65));
+            slot[8] = new Slot("BR",new Rectangle(286, 250, 65, 65));
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public Rectangle Rect
+        {
+            get { return rect; }
+            set { rect = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Vector2 Center
+        {
+            get { return center; }
+            set { center = value; }
+        }
+    }
+}
---
+++ Slot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TripleTriadOffline
+{
+    public class Slot
+    {
+        public Card cardSlotted;
+        public Slot(){}
+        public Slot(string Name, Rectangle Rect)
+        {
+            name = Name;
+            rect = Rect;
+            isOccupied = false;
+        }
+
+        public string name { get; set; }
+        public Rectangle rect { get; set; }
+        public bool isOccupied { get; set; }
+        public Card CardSlotted 
+        {
+            get {return cardSlotted;}
+            set {cardSlotted = value;}
+        }
+    }
+}
